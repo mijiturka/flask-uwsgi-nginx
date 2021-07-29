bb9b7b1f768b8b2cd8f5099514422fb62ab062e7
Rumyana <mijiturka@tuta.io>
2021-07-30 00:28:51 +0100

    Reload nginx or start it if it reloading fails
    Allows the config/startup file for nginx to be used
    the first time around as well

--- nginx/configure.sh
+++ nginx/configure.sh
@@ -5,4 +5,4 @@ set -eux pipefail
 sed -i "s/{{FLASK_PORT}}/${FLASK_PORT}/g" settings.conf
 cp settings.conf /etc/nginx/sites-enabled/app
 rm /etc/nginx/sites-enabled/default
-service nginx reload
+service nginx reload || nginx
